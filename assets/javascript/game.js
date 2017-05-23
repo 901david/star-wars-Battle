747616da2e9695863b61a0fb79c072114e5e5822
h fam <h fam>
2017-05-23 05:40:22 -0600

    Initial layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+// Variable Definitions
+var attackPower = 8;
+var attackIncreased;
+
+
+
+
+
+
+
+
+// Display only the top text indicating to choose a character
+
+
+
+
+
+
+
+
+
+// When any of the boxes are clicked that one highlights, moves to left
+// plays the correct sound and the three other boxes move down below.  
+
+
+
+
+
+
+
+
+// Once the other boxes are below once you click the next choice
+// The chosen box will move below and other still stay above
+
+
+
+// Now you have an enemy. As you press attack the sound 
+// that is associated with the add 8 to yoru attack each time
+// while storing that value somewhere else.
