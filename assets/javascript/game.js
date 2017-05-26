ca26e4e8a13d2995a26131b3b657d69cb23618c1
h fam <h fam>
2017-05-26 13:06:05 -0600

    Corrected computer recognizing defender character and counting attack power and health of both characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,15 @@
 // Variable Definitions
 var currentCharacter;
-var currentDefender;
-var enemyChosen = false;
-var defeated = false;
-var currentAttackPower = 0;
+var currentDefenderHealth;
+var currentDefenderAttack;
+var defeated = 0;
+var currentAttackPower;
 var currentHealth = 0;
+var currentEnemyName;
+var baseAttackPower;
+var increaseAttack;
+var nameValue;
+
 
 // Object declaration to define characters
 var characters = {
@@ -18,7 +23,8 @@ var characters = {
 			chooseSound: new Audio('assets/audio/chewy-select.mp3'),
 			attackSound: new Audio('assets/audio/chewy-attack.mp3'),
 			winSound:new Audio('assets/audio/chewy-win.mp3'),
-			picked: false
+			picked: false,
+			value: "chewy"
 				},
 		palp: {
 			name: "Emperor Palpatine",
@@ -30,7 +36,8 @@ var characters = {
 			chooseSound: new Audio('assets/audio/palp-select.mp3'),
 			attackSound: new Audio('assets/audio/palp-attack.wav'),
 			winSound:new Audio('#'),
-			picked: false
+			picked: false,
+			value: "palp"
 				},
 		leia: {
 			name: "Princess Leia",
@@ -42,7 +49,8 @@ var characters = {
 			chooseSound: new Audio('assets/audio/leia-select.mp3'),
 			attackSound: new Audio('assets/audio/leia-attack.mp3'),
 			winSound:new Audio('#'),
-			picked: false
+			picked: false,
+			value: "leia"
 				},
 		vader: {
 			name: "Darth Vader",
@@ -54,23 +62,29 @@ var characters = {
 			chooseSound: new Audio('assets/audio/vader-select.mp3'),
 			attackSound: new Audio('assets/audio/vader-attack.wav'),
 			winSound:new Audio('assets/audio/vader-win.mp3'),
-			picked: false
+			picked: false,
+			value: "vader"
 				}
 		}
 
 // Function to generate a character box
 function generateCharacter (x, y) {
-	var characterBox = $('<div class="funBox"><p>' + characters[y]["name"] + '</p>' + characters[y]["image"] + '<p>' + characters[y]["health"] + '</p></div>');
+	var characterBox = $('<div class="funBox" value="' + characters[y]["value"] + '"><p>' + characters[y]["name"] + '</p>' + characters[y]["image"] + '<p>' + characters[y]["health"] + '</p></div>');
 		$(x).append(characterBox);
 		$(characterBox).one("click", function () {
 		$(".startUpAudio").attr("src", "");
 		$("#secondRow").append(this);
 		$("#secondRow").off("click");
 		characters[y]["chooseSound"].play();
-		currentAttackPower = characters[y]["baseAttack"];
-		console.log(currentAttackPower);
+		currentCharacter = characters[y]["name"];
+		console.log(currentCharacter);
+		baseAttackPower = characters[y]["baseAttack"];
+		increaseAttack = characters[y]["attack"];
+		console.log(baseAttackPower);
 		currentHealth = characters[y]["health"];
 		console.log(currentHealth);
+		currentAttackPower = baseAttackPower;
+		console.log(currentAttackPower);
 		$("#secondRow").prepend("<h3 class='startText'>Your Character</h3>");
 		$("#thirdRow").append("<h3 class='startText'>Enemies Available to Choose</h3>");
 		$("#firstRow .funBox").off("click");
@@ -78,15 +92,16 @@ function generateCharacter (x, y) {
 		$("#thirdRow").append($("#firstRow .funBox"));
 		$("#firstRow").empty();
 		$("#thirdRow .funBox").one("click", function () {
-	
 			$("#thirdRow .funBox").off("click");
 			$("#fourthRow").append('<h3 class="moveDown">Fight Section</h3><button class="btn-danger btn-group-lg">Attack</button><h3>Defender Character</h3>')
 			$("#fourthRow").append(this);
+			nameValue = $('#fourthRow div').attr('value');
+			console.log(nameValue);
+			whichDefender();
 			$("#fourthRow .funBox").addClass("backBlack");
-			currentDefender = $("#fourthRow .funBox");
-			console.log(currentDefender);
 			$(".btn-danger").on("click", function() {
 			characters[y].attackSound.play();
+			attackDefender();
 
 
 		});
@@ -98,9 +113,49 @@ function generateCharacter (x, y) {
 	});
 };
 // This function should control your health and the enemy health as well as the text across bottom
-function pressAttack (name) {
+function whichDefender () {
+	if (nameValue ===  'vader') {
+		currentEnemyName = "Darth Vader";
+		currentDefenderHealth = 180;
+		currentDefenderAttack = 15;
+		console.log(currentDefenderAttack)
+		console.log(currentDefenderHealth)
+		console.log(currentEnemyName);
+	}
+	else if (nameValue ===  'leia') {
+		currentEnemyName = "Princess Leia";
+		currentDefenderHealth = 100;
+		currentDefenderAttack = 5;
+		console.log(currentDefenderAttack)
+		console.log(currentDefenderHealth)
+		console.log(currentEnemyName);
+	}
+	else if (nameValue ===  'palp') {
+		currentEnemyName = "Emperor Palpatine";
+		currentDefenderHealth = 150;
+		currentDefenderAttack = 12;
+		console.log(currentDefenderAttack)
+		console.log(currentDefenderHealth)
+	}
+	else if (nameValue ===  'chewy') {
+		currentEnemyName = "Chewbaca";
+		currentDefenderHealth = 120;
+		currentDefenderAttack = 10;
+		console.log(currentDefenderAttack)
+		console.log(currentDefenderHealth)
+		console.log(currentEnemyName);
+	}
 
 };
+function attackDefender () {
+	currentHealth = currentHealth - currentDefenderAttack;
+	currentDefenderHealth = currentDefenderHealth - currentAttackPower;
+	currentAttackPower = currentAttackPower + increaseAttack;
+	console.log("Att Power " + currentAttackPower);
+	console.log("Char Health " + currentHealth);
+	console.log("Def Health " + currentDefenderHealth);
+	$(".bottomText").html("<p>You attacked " + currentEnemyName + " for " + currentAttackPower + " damage.</p>" + "<p>" + currentEnemyName + " attacked you back for " + currentDefenderAttack + " damage.</p>");
+}
 
 $(document).ready(function(){
 	//This will control autoplay of Theme once you get to site.
