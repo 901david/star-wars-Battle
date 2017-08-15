d7438adb1007baab2a30d0125db36b6ddbc233bd
DHLAP\h <vdavidhammond@gmail.com>
2017-08-15 16:09:27 -0600

    Cleaned up the code some

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,86 +1,82 @@
 // Variable Definitions
-var currentCharacter;
 var currentDefenderHealth;
 var currentDefenderAttack;
 var defeated = 0;
 var currentAttackPower;
 var currentHealth = 0;
 var currentEnemyName;
-var baseAttackPower;
 var increaseAttack;
 var nameValue;
 var characterWinSound;
 // Object declaration to define characters/win sounds etc
 var characters = {
-		chewy: {
-			name: "Chewbaca",
-			image: '<img src="assets/images/chewy.jpg" alt="Chewbaca" class="chewy">',
-			baseAttack: 12,
-			attack: 12,
-			counterAttack: 14, 
-			health: 145,
-			chooseSound: new Audio('assets/audio/chewy-select.mp3'),
-			attackSound: new Audio('assets/audio/chewy-attack.mp3'),
-			winSound:new Audio('assets/audio/chewy-win.mp3'),
-			value: "chewy"
-				},
-		palp: {
-			name: "Emperor Palpatine",
-			image: '<img src="assets/images/palp.png" alt="Emperor Palpatine" class="palp">',
-			baseAttack: 10,
-			attack: 10,
-			counterAttack: 16, 
-			health: 150,
-			chooseSound: new Audio('assets/audio/palp-select.mp3'),
-			attackSound: new Audio('assets/audio/palp-attack.wav'),
-			winSound:new Audio('assets/audio/palp-win.WAV'),
-			value: "palp"
-				},
-		leia: {
-			name: "Princess Leia",
-			image: '<img src="assets/images/leia.jpg" alt="Princess Leia" class="leia">',
-			baseAttack: 11,
-			attack: 11,
-			counterAttack: 15, 
-			health: 155,
-			chooseSound: new Audio('assets/audio/leia-select.mp3'),
-			attackSound: new Audio('assets/audio/leia-attack.mp3'),
-			winSound:new Audio('assets/audio/leia-win.mp3'),
-			value: "leia"
-				},
-		vader: {
-			name: "Darth Vader",
-			image: '<img src="assets/images/vader.jpg" alt="Darth Vader" class="vader">',
-			baseAttack: 13,
-			attack: 13,
-			counterAttack: 17, 
-			health: 135,
-			chooseSound: new Audio('assets/audio/vader-select.mp3'),
-			attackSound: new Audio('assets/audio/vader-attack.wav'),
-			winSound:new Audio('assets/audio/vader-win.mp3'),
-			value: "vader"
-				},
-		}
+	chewy: {
+		name: "Chewbaca",
+		image: '<img src="assets/images/chewy.jpg" alt="Chewbaca" class="chewy">',
+		baseAttack: 12,
+		attack: 12,
+		counterAttack: 14,
+		health: 145,
+		chooseSound: new Audio('assets/audio/chewy-select.mp3'),
+		attackSound: new Audio('assets/audio/chewy-attack.mp3'),
+		winSound:new Audio('assets/audio/chewy-win.mp3'),
+		value: "chewy"
+	},
+	palp: {
+		name: "Emperor Palpatine",
+		image: '<img src="assets/images/palp.png" alt="Emperor Palpatine" class="palp">',
+		baseAttack: 10,
+		attack: 10,
+		counterAttack: 16,
+		health: 150,
+		chooseSound: new Audio('assets/audio/palp-select.mp3'),
+		attackSound: new Audio('assets/audio/palp-attack.wav'),
+		winSound:new Audio('assets/audio/palp-win.WAV'),
+		value: "palp"
+	},
+	leia: {
+		name: "Princess Leia",
+		image: '<img src="assets/images/leia.jpg" alt="Princess Leia" class="leia">',
+		baseAttack: 11,
+		attack: 11,
+		counterAttack: 15,
+		health: 155,
+		chooseSound: new Audio('assets/audio/leia-select.mp3'),
+		attackSound: new Audio('assets/audio/leia-attack.mp3'),
+		winSound:new Audio('assets/audio/leia-win.mp3'),
+		value: "leia"
+	},
+	vader: {
+		name: "Darth Vader",
+		image: '<img src="assets/images/vader.jpg" alt="Darth Vader" class="vader">',
+		baseAttack: 13,
+		attack: 13,
+		counterAttack: 17,
+		health: 135,
+		chooseSound: new Audio('assets/audio/vader-select.mp3'),
+		attackSound: new Audio('assets/audio/vader-attack.wav'),
+		winSound:new Audio('assets/audio/vader-win.mp3'),
+		value: "vader"
+	},
+}
 // Function to generate a character box and begin pushing us through the majority of the game
 function generateCharacter (x, y) {
-		// Initializes the characters on the screen
-		var characterBox = $('<div class="funBox" value="' + characters[y]["value"] + '"><p>' + characters[y]["name"] + '</p>' + characters[y]["image"] + '<p class="health">' + characters[y]["health"] + '</p></div>').show().fadeIn(3000);
-		$(x).append(characterBox);
-		$(characterBox).one("click", function () {
+	let currentCharacter;
+	let baseAttackPower;
+	// Initializes the characters on the screen
+	var characterBox = $('<div class="funBox" value="' + characters[y]["value"] + '"><p>' + characters[y]["name"] + '</p>' + characters[y]["image"] + '<p class="health">' + characters[y]["health"] + '</p></div>').show().fadeIn(3000);
+	$(x).append(characterBox);
+	$(characterBox).one("click", function () {
 		$(".startUpAudio").attr("src", "");
 		$("#secondRow").append(this);
 		characters[y]["chooseSound"].play();
 		// Storing info about character selection to be used later
 		currentCharacter = characters[y]["name"];
-		console.log(currentCharacter);
 		characterWinSound = characters[y]["winSound"];
 		baseAttackPower = characters[y]["baseAttack"];
 		increaseAttack = characters[y]["attack"];
-		console.log(baseAttackPower);
 		currentHealth = characters[y]["health"];
-		console.log(currentHealth);
 		currentAttackPower = baseAttackPower;
-		console.log(currentAttackPower);
 		// Starts defining the next row of characters which are now enemies
 		$("#secondRow").prepend("<h3 class='startText'>Your Character</h3>");
 		$("#thirdRow").append("<h3 class='startText'>Enemies Available to Choose</h3>");
@@ -90,25 +86,21 @@ function generateCharacter (x, y) {
 		$("#firstRow").empty();
 		// Defines what happens when you click on an enemy
 		$("#thirdRow .backRed").on("click", function () {
-			
+
 			if (currentEnemyName == undefined) {
-			$("#fourthRow").append('<h3 class="moveDown">Fight Section</h3><button class="btn-danger btn-group-lg">Attack</button><h3>Defender Character</h3>').show().fadeIn(1000);
-			$("#fourthRow").append(this);
-			nameValue = $('#fourthRow div').attr('value');
-			console.log(nameValue);
-			currentEnemyName = characters[nameValue]["name"];
-			currentDefenderHealth = characters[nameValue]["health"];
-			currentDefenderAttack = characters[nameValue]["counterAttack"];
-			console.log(currentDefenderAttack)
-			console.log(currentDefenderHealth)
-			console.log(currentEnemyName);
-			$("#fourthRow .funBox").addClass("backBlack");
-			$(".btn-danger").on("click", function() {
-			characters[y].attackSound.play();
-			attackDefender();
+				$("#fourthRow").append('<h3 class="moveDown">Fight Section</h3><button class="btn-danger btn-group-lg">Attack</button><h3>Defender Character</h3>').show().fadeIn(1000);
+				$("#fourthRow").append(this);
+				nameValue = $('#fourthRow div').attr('value');
+				currentEnemyName = characters[nameValue]["name"];
+				currentDefenderHealth = characters[nameValue]["health"];
+				currentDefenderAttack = characters[nameValue]["counterAttack"];
+				$("#fourthRow .funBox").addClass("backBlack");
+				$(".btn-danger").on("click", function() {
+					characters[y].attackSound.play();
+					attackDefender();
+				});
+			};
 		});
-		};
-	});
 	});
 };
 // This function dictates what happens when attack button pressed
@@ -116,9 +108,6 @@ function attackDefender () {
 	currentHealth = currentHealth - currentDefenderAttack;
 	currentDefenderHealth = currentDefenderHealth - currentAttackPower;
 	currentAttackPower = currentAttackPower + increaseAttack;
-	console.log("Att Power " + currentAttackPower);
-	console.log("Char Health " + currentHealth);
-	console.log("Def Health " + currentDefenderHealth);
 	$("#fifthRow").html("<p>You attacked " + currentEnemyName + " for " + currentAttackPower + " damage.</p>" + "<p>" + currentEnemyName + " attacked you back for " + currentDefenderAttack + " damage.</p>");
 	$("#fourthRow .health").html(currentDefenderHealth);
 	$("#secondRow .health").html(currentHealth);
@@ -132,20 +121,19 @@ function checkGameStatus () {
 		$("#fourthRow").empty();
 		if (defeatedEnemy) {
 			defeated++;
-			console.log("Defeated" + defeated);
-		$("#fifthRow").html("<p>You defeated " + currentEnemyName + ".</p><p>Click another enemy to continue.</p>");
-		currentEnemyName = undefined;
+			$("#fifthRow").html("<p>You defeated " + currentEnemyName + ".</p><p>Click another enemy to continue.</p>");
+			currentEnemyName = undefined;
 
-};
+		};
 
-}
+	}
 	if (defeated >= 3) {
 		$("#backG").append("<audio class='startUpAudio' src='assets/audio/win-overall-song.mp3' autoplay></audio>");
 		$("#fifthRow").html("<p>YOU WIN!!!</p><p>The Force is strong with you.  Press reset to try again</p>")
 		$("#fifthRow").append("<button class='btn-warning btn-group-lg'>Reset</button>");
 		$(".btn-warning").on("click", function() {
 			location.reload(true);
-	});
+		});
 
 	};
 	if (currentHealth <= 0) {
@@ -154,7 +142,7 @@ function checkGameStatus () {
 		$("#fifthRow").append("<button class='btn-warning btn-group-lg'>Reset</button>");
 		$(".btn-warning").on("click", function() {
 			location.reload(true);
-	});
+		});
 	};
 };
 
@@ -170,8 +158,6 @@ $(document).ready(function(){
 	generateCharacter("#firstRow", "palp");
 	generateCharacter("#firstRow", "vader");
 	$("#firstRow").append(makeAChoice);
-	
-	
-	});
-	
 
+
+});
